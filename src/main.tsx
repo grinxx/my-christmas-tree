f85e1dc0f4e9b6869a500ca1ae71991549d1b2b3
lovfam <3199574656@qq.com>
2025-12-05 21:14:02 +0800

    last version

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+// import TikTokApp from './TikTokApp.tsx' // 2. 引入新的 TikTokApp (注意文件名)
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        { <App /> }
+    </StrictMode>
+    // <TikTokApp />     
+
 )
